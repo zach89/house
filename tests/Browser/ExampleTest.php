067b5ef63f07b1da39ff6672c0d4de09cbf017d6
root <zach88@163.com>
2017-02-13 09:49:26 +0800

    master

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -16,7 +16,7 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
+            $browser->visit('/home')
                     ->assertSee('Laravel');
         });
     }
